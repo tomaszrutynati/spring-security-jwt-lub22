7e5f00984d3264929cc007fed9c9abd211b8fcb5
Tomasz.Rutyna <Poniatowa2021!>
2020-11-07 13:00:57 +0100

    init commit

---
+++ src/main/java/pl/sda/jwtlub22/SpringSecurityJwtLub22Application.java
@@ -0,0 +1,13 @@
+package pl.sda.jwtlub22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityJwtLub22Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityJwtLub22Application.class, args);
+    }
+
+}
